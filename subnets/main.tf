900ef63aa9b3136db8f069642c1cd05bb46a8239
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-02-23 16:44:58 +0530

    advanced subnets

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,1 +1,11 @@
 
+module "subnets" {
+  source                    = "./subnets"
+  availability_zone         = var.availability_zone
+  cidr_block                = var.cidr_block
+  default_vpc_id            = var.default_vpc_id
+  env                       = var.env
+  name                      = var.name
+  vpc_id                    = var.vpc_id
+  vpc_peering_connection_id = var.vpc_peering_connection_id
+}
